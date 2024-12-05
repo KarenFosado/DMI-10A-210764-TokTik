23446531de99004a091ec1010c77fb5131ff1971
Karen <210764@utxicotepec.edu.mx>
2024-12-04 20:38:42 -0600

    coreecion de errores

--- toktik/lib/infraestructure/models/local_video_model.dart
+++ toktik/lib/infraestructure/models/local_video_model.dart
@@ -6,23 +6,25 @@ class LocalVideoModel {
   final int likes;
   final int views;
 
-  LocalVideoModel(
-      {required this.name,
-      required this.videoUrl,
-      this.likes = 0,
-      this.views = 0});
+  LocalVideoModel({
+    required this.name,
+    required this.videoUrl,
+    this.likes = 0,
+    this.views = 0,
+  });
 
   factory LocalVideoModel.fromJson(Map<String, dynamic> json) =>
       LocalVideoModel(
         name: json["name"] ?? 'No name',
-        videoUrl: json['videoUrl'],
-        likes: json['likes'] ?? 0,
-        views: json['views'] ?? 0,
+        videoUrl: json["videoUrl"] ?? 'No se encontro el video',
+        likes: json["likes"] ?? 0,
+        views: json["views"] ?? 0,
       );
 
   VideoPost toVideoPostEntity() => VideoPost(
-        caption: name, 
-        videoUrl: videoUrl, 
-        likes: likes, 
-        views: views );
+    caption: name, 
+    videoUrl: videoUrl,
+    likes: likes,
+    views: views
+    );
 }
